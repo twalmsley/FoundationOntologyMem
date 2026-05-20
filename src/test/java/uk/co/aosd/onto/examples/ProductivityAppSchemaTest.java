d8125a01dcd94b8723f4a7c0bf974f572b40ebf3
Tony <tony@aosd.co.uk>
2026-05-20 07:59:55 +0100

    Minor changes to some example code.

--- src/test/java/uk/co/aosd/onto/examples/ProductivityAppSchemaTest.java
+++ src/test/java/uk/co/aosd/onto/examples/ProductivityAppSchemaTest.java
@@ -29,52 +29,50 @@ public class ProductivityAppSchemaTest {
 
     @Test
     public void testCreateUser() {
-        final Instant userCreatedDate = Instant.parse("2026-05-19T07:00:00.00Z");
-        final ResignifiedImpl userNamed = new ResignifiedImpl("18d80104-d51b-456a-afc9-0a88957d270c", userCreatedDate, userCreatedDate);
-        final ResignifiedImpl userRenamed = new ResignifiedImpl("bfe57f9f-ae82-46b6-8fe3-56a626cde281", null, null);
-        final ResignifiedImpl emailCreated = new ResignifiedImpl("e1a51459-80d1-4482-a9e2-0a737d0beda0", null, null);
-        final ResignifiedImpl emailChanged = new ResignifiedImpl("50803b80-38df-4b5d-bcea-d39b850dfc96", null, null);
+        final var userCreatedDate = Instant.parse("2026-05-19T07:00:00.00Z");
+        final var userNamed = new ResignifiedImpl("18d80104-d51b-456a-afc9-0a88957d270c", userCreatedDate, userCreatedDate);
+        final var userRenamed = new ResignifiedImpl("bfe57f9f-ae82-46b6-8fe3-56a626cde281", null, null);
+        final var emailCreated = new ResignifiedImpl("e1a51459-80d1-4482-a9e2-0a737d0beda0", null, null);
+        final var emailChanged = new ResignifiedImpl("50803b80-38df-4b5d-bcea-d39b850dfc96", null, null);
 
-        final LanguageImpl englishLanguage = new LanguageImpl("016c47f1-f394-40d2-8be1-26aa44667fca", "English");
-        final Class<LanguageImpl> userLanguages = new ClassImpl<>("88db6cc5-5ef7-4af4-b72d-629c21e92108", Set.of(englishLanguage));
+        final var englishLanguage = new LanguageImpl("016c47f1-f394-40d2-8be1-26aa44667fca", "English");
+        final var userLanguages = new ClassImpl<>("88db6cc5-5ef7-4af4-b72d-629c21e92108", Set.of(englishLanguage));
 
-        final CreatedImpl userCreated = new CreatedImpl("c92b4597-3c63-4c31-a615-5dcf26ae4976", userCreatedDate, userCreatedDate);
-        final DestroyedImpl userDesroyed = new DestroyedImpl("cf299d3b-4587-4b15-9e09-91969bb80db9", null, null);
-        final Signifier<String, ResignifiedImpl> username = new SignifierImpl<>("9bb91d39-5dab-44c1-8def-27b8605b4a27", "user1", englishLanguage, userNamed,
-            userRenamed);
-        final CreatedImpl emailAddressCreated = new CreatedImpl("75a84adf-f76d-48c4-95a7-f4cfd37d33d3", null, null);
-        final DestroyedImpl emailAddressDestroyed = new DestroyedImpl("586cc577-b17f-4de5-9244-2b6e54d4b0ba", null, null);
-        final EmailAddress mailAddress = new EmailAddress("user1@example.com", emailAddressCreated, emailAddressDestroyed);
-        final Signifier<EmailAddress, ResignifiedImpl> emailAddressSignifier = new SignifierImpl<>("bf3eaedc-aa4c-4a12-84e8-4a24e62ef232", mailAddress,
-            englishLanguage, emailCreated, emailChanged);
-        final Class<Signifier<String, ResignifiedImpl>> usernames = new ClassImpl<>("28864174-40c8-4c46-83d6-6444b0b3a20f", Set.of(username));
-        final Class<Signifier<EmailAddress, ResignifiedImpl>> userEmailAddresses = new ClassImpl<>("67c8ca75-f724-4d02-b35a-3efc2ae05060",
+        final var userCreated = new CreatedImpl("c92b4597-3c63-4c31-a615-5dcf26ae4976", userCreatedDate, userCreatedDate);
+        final var userDesroyed = new DestroyedImpl("cf299d3b-4587-4b15-9e09-91969bb80db9", null, null);
+        final var username = new SignifierImpl<>("9bb91d39-5dab-44c1-8def-27b8605b4a27", "user1", englishLanguage, userNamed, userRenamed);
+        final var emailAddressCreated = new CreatedImpl("75a84adf-f76d-48c4-95a7-f4cfd37d33d3", null, null);
+        final var emailAddressDestroyed = new DestroyedImpl("586cc577-b17f-4de5-9244-2b6e54d4b0ba", null, null);
+        final var mailAddress = new EmailAddress("user1@example.com", emailAddressCreated, emailAddressDestroyed);
+        final var emailAddressSignifier = new SignifierImpl<>("bf3eaedc-aa4c-4a12-84e8-4a24e62ef232", mailAddress, englishLanguage, emailCreated, emailChanged);
+        final var usernames = new ClassImpl<Signifier<String, ResignifiedImpl>>("28864174-40c8-4c46-83d6-6444b0b3a20f", Set.of(username));
+        final var userEmailAddresses = new ClassImpl<Signifier<EmailAddress, ResignifiedImpl>>("67c8ca75-f724-4d02-b35a-3efc2ae05060",
             Set.of(emailAddressSignifier));
-        final BooleanValueImpl<ProStatus> userProStatus = new BooleanValueImpl<>(Boolean.TRUE, ProStatus.PRO_STATUS);
-        final ScalarValueImpl<Integer, Minutes> focusDuration = new ScalarValueImpl<>(25, Units.MINUTES);
-        final ScalarValueImpl<Integer, Minutes> shortBreakDuration = new ScalarValueImpl<>(5, Units.MINUTES);
-        final ScalarValueImpl<Integer, Minutes> longBreakDuration = new ScalarValueImpl<>(15, Units.MINUTES);
-        final ScalarValueImpl<Integer, Natural> longBreakInterval = new ScalarValueImpl<>(4, Units.NATURAL);
-        final Theme theme = Theme.LIGHT;
-        final Class<Task> tasks = new ClassImpl<>("1710fa1d-b329-43d4-9a05-e3dcb6233c91", Set.of());
-        final Class<Project> projects = new ClassImpl<>("d8658d70-3d34-4348-974d-d4dd9a69ea07", Set.of());
+        final var userProStatus = new BooleanValueImpl<ProStatus>(Boolean.TRUE, ProStatus.PRO_STATUS);
+        final var focusDuration = new ScalarValueImpl<Integer, Minutes>(25, Units.MINUTES);
+        final var shortBreakDuration = new ScalarValueImpl<Integer, Minutes>(5, Units.MINUTES);
+        final var longBreakDuration = new ScalarValueImpl<Integer, Minutes>(15, Units.MINUTES);
+        final var longBreakInterval = new ScalarValueImpl<Integer, Natural>(4, Units.NATURAL);
+        final var theme = Theme.LIGHT;
+        final var tasks = new ClassImpl<Task>("1710fa1d-b329-43d4-9a05-e3dcb6233c91", Set.of());
+        final var projects = new ClassImpl<Project>("d8658d70-3d34-4348-974d-d4dd9a69ea07", Set.of());
 
-        final StartedImpl startOfSubscription = new StartedImpl("3b07b21e-0823-48f7-8f56-6ecbd1554137", userCreatedDate, userCreatedDate);
-        final StoppedImpl endOfSubscription = new StoppedImpl("017047f4-b188-464d-a126-51bd89f744bf", null, null);
-        final SubscriptionLevel subscriptionLevel = SubscriptionLevel.PREMIUM;
-        final SubscriptionStatus subscriptionStatus = SubscriptionStatus.FREE_TRIAL;
-        final SubscriptionType subscriptionType = SubscriptionType.YEARLY;
-        final String subscriptionPlan = "FREE_TRIAL";
-        final Instant freeTrialExpiresAt = Instant.parse("2026-05-31T07:00:00.00Z");
-        final String stripeCustomerId = "3b0c4ec3-a0bb-4569-95df-65ef7d8545ec";
-        final UserSubscription userSubscription = new UserSubscription("2ef52355-9ffe-4d78-a0f7-ae3f077b622b", startOfSubscription, endOfSubscription,
-            subscriptionStatus, subscriptionType, subscriptionLevel, subscriptionPlan, freeTrialExpiresAt, stripeCustomerId);
-        final Class<UserSubscription> subscriptions = new ClassImpl<>("1ab98632-ed54-4d02-9999-53017f190caf", Set.of(userSubscription));
-        final Class<UserSession> sessions = new ClassImpl<>("26bcb866-dd60-47fa-9f5f-e975b102a493", Set.of());
-        final Class<JournalEntry> journalEntries = new ClassImpl<>("a26cd567-0530-464e-961e-c45951ba43d1", Set.of());
-        final Class<CyclicTask> cyclicTasks = new ClassImpl<>("e021c634-ccaf-4988-96a0-249066aee542", Set.of());
-        final Class<Tracker> trackers = new ClassImpl<>("b79357f1-9209-4d86-91ba-33cbb559e896", Set.of());
-        final Class<Report> reports = new ClassImpl<>("60bbf872-2bf2-426b-a15e-97e1fd654711", Set.of());
+        final var startOfSubscription = new StartedImpl("3b07b21e-0823-48f7-8f56-6ecbd1554137", userCreatedDate, userCreatedDate);
+        final var endOfSubscription = new StoppedImpl("017047f4-b188-464d-a126-51bd89f744bf", null, null);
+        final var subscriptionLevel = SubscriptionLevel.PREMIUM;
+        final var subscriptionStatus = SubscriptionStatus.FREE_TRIAL;
+        final var subscriptionType = SubscriptionType.YEARLY;
+        final var subscriptionPlan = "FREE_TRIAL";
+        final var freeTrialExpiresAt = Instant.parse("2026-05-31T07:00:00.00Z");
+        final var stripeCustomerId = "3b0c4ec3-a0bb-4569-95df-65ef7d8545ec";
+        final var userSubscription = new UserSubscription("2ef52355-9ffe-4d78-a0f7-ae3f077b622b", startOfSubscription, endOfSubscription, subscriptionStatus,
+            subscriptionType, subscriptionLevel, subscriptionPlan, freeTrialExpiresAt, stripeCustomerId);
+        final var subscriptions = new ClassImpl<UserSubscription>("1ab98632-ed54-4d02-9999-53017f190caf", Set.of(userSubscription));
+        final var sessions = new ClassImpl<UserSession>("26bcb866-dd60-47fa-9f5f-e975b102a493", Set.of());
+        final var journalEntries = new ClassImpl<JournalEntry>("a26cd567-0530-464e-961e-c45951ba43d1", Set.of());
+        final var cyclicTasks = new ClassImpl<CyclicTask>("e021c634-ccaf-4988-96a0-249066aee542", Set.of());
+        final var trackers = new ClassImpl<Tracker>("b79357f1-9209-4d86-91ba-33cbb559e896", Set.of());
+        final var reports = new ClassImpl<Report>("60bbf872-2bf2-426b-a15e-97e1fd654711", Set.of());
 
         final var user = new User(
             "272e105c-5c06-4330-b454-9b5b73705250",
